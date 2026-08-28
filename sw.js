481cc5ca4aa466bead69c236990ae595913bf905
blazyca62-wq <blazyca62@gmail.com>
2026-08-29 00:33:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='atelierai-ipad-v1-20-11-point-directsplay-heller';
+const CACHE='atelierai-ipad-v1-20-13-pipette-mixfixsplay-heller';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([
