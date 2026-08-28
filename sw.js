8cecf099b32326a90ce8516db6579d655c7fdf09
blazyca62-wq <blazyca62@gmail.com>
2026-08-29 00:51:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,2 @@
-const CACHE='atelierai-ipad-v1-20-8a-only-swatchessplay-heller';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(Promise.all([
- caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
- self.clients.claim()
-]))});
-self.addEventListener('fetch',e=>{
- if(e.request.mode==='navigate'){
-   e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));
-   return;
- }
- e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
-});
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(self.registration.unregister()));
